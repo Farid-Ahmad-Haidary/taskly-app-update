f4921be02159b3472f107bb9eb9bb3b019a6c4ae
Farid Ahmad Haidary <faridahmadhaidary01@gmail.com>
2026-04-16 23:13:47 +0430

    tailwind CSS is linked

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home page</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
+
 <body>
-    <h1>
-        Welcome to Taskly App
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
     </h1>
-    
 </body>
+
 </html>
