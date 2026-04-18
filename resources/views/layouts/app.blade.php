c41ad381e4ce5faea19031703fa16ede893b69e3
Farid Ahmad Haidary <faridahmadhaidary01@gmail.com>
2026-04-18 23:34:45 +0430

    Changes brought to layout and home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,1 +1,20 @@
+<!doctype html>
+<html>
 
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    {{-- Tailwind Css CDN link --}}
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    {{-- Font Awsome Link --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free/css/all.min.css">
+    <title>Taskly App</title>
+</head>
+
+<body>
+
+
+
+</body>
+
+</html>
